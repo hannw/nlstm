3beb207e78a1faa46f1e6a08269559678f2b25c2
hannw <lostkingwwwth@gmail.com>
2018-02-02 18:32:04 -0800

    delete redundent test

--- rnn_cell_test.py
+++ rnn_cell_test.py
@@ -10,7 +10,7 @@ class TestNLSTM(tf.test.TestCase):
 
   def test_nlstm_tuple(self):
     batch_size = 2
-    cell = rc.NLSTMCell(num_units=3, depth=2)
+    cell = rc.NLSTMCell(num_units=3, depth=4)
     init_state = cell.zero_state(batch_size, dtype=tf.float32)
     output, new_state = cell(inputs=tf.random_normal([batch_size, 5]), state=init_state)
 
@@ -22,6 +22,7 @@ class TestNLSTM(tf.test.TestCase):
     self.assertAllEqual(vals[0].shape, [2, 3])
     for val in vals[1]:
       self.assertAllEqual(val.shape, [2, 3])
+    self.assertEqual(len(vals[1]), 5)
 
   def test_nlstm_non_tuple(self):
     batch_size = 2
@@ -36,21 +37,6 @@ class TestNLSTM(tf.test.TestCase):
     self.assertAllEqual(vals[0], vals[1][:, :3])
     self.assertAllEqual(vals[0].shape, [2, 3])
     self.assertAllEqual(vals[1].shape, [2, 9])
-
-  def test_nlstm_tuple(self):
-    batch_size = 2
-    cell = rc.NLSTMCell(num_units=3, depth=2)
-    init_state = cell.zero_state(batch_size, dtype=tf.float32)
-    output, new_state = cell(inputs=tf.random_normal([batch_size, 5]), state=init_state)
-
-    with self.test_session() as sess:
-      tf.global_variables_initializer().run()
-      vals = sess.run([output, new_state])
-
-    self.assertAllEqual(vals[0], vals[1][0])
-    self.assertAllEqual(vals[0].shape, [2, 3])
-    for val in vals[1]:
-      self.assertAllEqual(val.shape, [2, 3])
 
 
 
